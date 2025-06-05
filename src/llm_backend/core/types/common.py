5f0949456a02bba4e2214c68d96ac247d798cecb
Pxlart Design <michael.rhema@gmail.com>
2025-06-05 18:58:00 +0000

    Switch log_id type to str

--- src/llm_backend/core/types/common.py
+++ src/llm_backend/core/types/common.py
@@ -11,7 +11,7 @@ class RunInput(BaseModel):
     agent_email: str
     session_id: str
     message_type: str
-    log_id: int = None
+    log_id: str = None
     document_url: Optional[str] = None
     agent_tool_config: Optional[dict]
     selected_llm: Optional[dict] = None
